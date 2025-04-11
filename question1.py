7bc2843bdf12af2b14fc3ee275dc2e4a9d7d82e4
joetes98 <joseph.tesoriero@ucdenver.edu>
2025-04-11 15:09:48 -0400

    WIP

--- question1.py
+++ question1.py
@@ -30,6 +30,7 @@ def countingSort(arr, digit):
     for i in range(0, length):
         arr[i] = output[i]
 
+
 def radixSort(arr):
     maxValue = max(arr)
 
@@ -37,6 +38,7 @@ def radixSort(arr):
     while maxValue // digit > 0:
         countingSort(arr, digit)
         digit *= 10
+
 
 def insertionSort(A):
     # count = 0
@@ -46,10 +48,11 @@ def insertionSort(A):
         while i >= 0 and A[i] > key:
             A[i+1] = A[i]
             i -=1
-            count +=1
+            # count +=1
         A[i+1] = key
     # print(f"Number of comparisons: {count}")
     # return count
+
 
 def bucketSort(array):
     # create empty buckets
@@ -68,6 +71,15 @@ def bucketSort(array):
     for n in buckets:
         insertionSort(n)
 
+    # insert the sorted elements into the original array
+    count = 0
+    for i in range(len(buckets)):
+        for j in range(len(buckets[i])):
+            array[count] = buckets[i][j]
+            count += 1
+    
+    return array
+
 def isSorted(arr): # check if array is sorted in ascending order
     for i in range(1, len(arr)):
         if arr[i-1] > arr[i]:
@@ -76,7 +88,7 @@ def isSorted(arr): # check if array is sorted in ascending order
 
 '''
 
-Min-Heap Algorithms
+Min-Heap
 
 '''
 
@@ -96,10 +108,29 @@ def minHeapify(array, i):
         array[i][0], array[smallest][0] = array[smallest][0], array[i][0]
         minHeapify(array, smallest)   # largest == new i
 
+
 def buildMinHeap(array):
     for i in reversed(range(len(array)//2)):
         minHeapify(array, i)
 
+
+def is_min_heap(arr):
+    n = len(arr)
+    
+    # check all but leaf nodes nodes
+    for i in range(n // 2):
+        left = 2 * i + 1
+        right = 2 * i + 2
+        
+        # check if min heap condition is violated for left child
+        if left < n and arr[i][0] > arr[left][0]:
+            return False
+        
+        # check if min heap condition is violated for right child
+        if right < n and arr[i][0] > arr[right][0]:
+            return False
+    
+    return True
 
 '''
 Merge K Sorted Lists
@@ -131,6 +162,10 @@ def main():
     radixSort(data)
     print(data)
     print(isSorted(data))
+
+    data2 = random.sample(range(1000000), 1000)
+    bucketSort(data2)
+    print(isSorted(data2))
 
 
 if __name__ == '__main__':
